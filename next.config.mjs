41f0cec849e3d57877a4743a3356e8e484096fe9
Calebe <calebeluis17@gmail.com>
2025-08-04 20:14:47 -0300

    2.0.1

--- next.config.mjs
+++ next.config.mjs
@@ -9,23 +9,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Adicione estas configurações para rotas API
-  experimental: {
-    skipMiddlewareUrlNormalize: true,
-    skipTrailingSlashRedirect: true,
-  },
-  // Configuração para rotas de API
-  api: {
-    externalResolver: true,
-    bodyParser: false,
-  },
-  // Opcional: Ignora verificação de tipos durante o build para rotas API
+  // Adicione esta configuração para o Prisma
   webpack: (config) => {
-    config.infrastructureLogging = {
-      level: 'error',
-    };
+    config.externals = [...(config.externals || []), '@prisma/client'];
     return config;
-  },
+  }
 };
 
 export default nextConfig;
